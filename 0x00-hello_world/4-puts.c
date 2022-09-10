b89fa1926fe57025b5ea7fe2e415c62426528021
geekyrockstar <muhammedadegbola@gmail.com>
2022-09-10 13:29:17 -0700

    return with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ *  main - Prints quote
+ *  
+ *  Return: zero on success
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
