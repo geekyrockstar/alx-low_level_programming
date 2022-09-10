ab1b1c1d8696649a8a33b7e407521d55ad6c5b66
geekyrockstar <muhammedadegbola@gmail.com>
2022-09-10 13:49:59 -0700

    fix puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- *  main: Prints quote
- *  
- *  Return: zero on success
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
