a2e59c9df5428e9433284a636ebcb42f808b468f
muhammed <muhammed>
2022-09-17 04:37:28 +0100

    0-main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - test function to print put correct output
+ *
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	int num;
+
+	num = 1;
+
+	positive_or_negative(num);
+
+	return (0);
+}
